503ba8cf25278684260178958905d6032ad522d2
mikkel madsen <aebeloe.madsen@hotmail.com>
2016-06-30 10:47:47 +0200

    Farvevalg

---
+++ IkeaTabletopApp/IkeaTabletopApplication/Singleton/FarveValgSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IkeaTabletopApplication.Singleton
+{
+    internal class FarveValgSingleton
+    {
+        private static FarveValgSingleton intance = new FarveValgSingleton();
+
+        public static FarveValgSingleton Instance
+        {
+            get { return intance; }
+        }
+
+        public List<int> ListFarveValg { get; set; }
+
+        private FarveValgSingleton()
+        {
+            ListFarveValg = new List<int>();
+        }
+    }
+
+}
